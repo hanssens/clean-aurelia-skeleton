1962b99d54175a264bb571ff3dde9a89b7cbd581
hanssens <github@hanssens.com>
2017-08-31 00:57:51 +0200

    Added + configured router

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import { autoinject } from 'aurelia-dependency-injection';
+import { Router, RouterConfiguration } from 'aurelia-router';
+import { Routes } from './infra/routes';
+
+@autoinject
 export class App {
-  message = 'Hello World!';
+  constructor(private router: Router) {
+  }
+
+  private configureRouter(config: RouterConfiguration, router: Router): void {
+    config.title = 'Clean Aurelia App';
+    config.map(Routes.maps());
+
+    this.router = router;
+  }
 }
---
+++ src/infra/routes.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:max-line-length */
+/* tslint:disable:object-literal-sort-keys */
+
+import { RouteConfig } from 'aurelia-router';
+
+/**
+ * Abstracted the application routes, used in App.ts, to a seperate file.
+ */
+export class Routes {
+    public static maps(): RouteConfig[] {
+        return <RouteConfig[]> [
+            // home/root
+            { route: ['', 'home'], name: 'home', moduleId: './views/home/home', nav: true, title: 'Dashboard' }
+        ];
+    }
+}
